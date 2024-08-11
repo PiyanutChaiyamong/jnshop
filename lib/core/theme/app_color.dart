450014f8836c87a4bcc085bab221a4c51e74a97c
Nick <nick_11-13@hotmail.com>
2024-08-11 12:20:37 +0700

    Sanyalak

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import '../constant/helper.dart';
 
 class AppColors {
-  static final primary = hexToColor('#00a3a5');
+  static final primary = hexToColor('#1cb00b');
 
-  static final secondary = hexToColor('#ffc000');
+  static final secondary = hexToColor('#ffffff');
   static const secondary2 = Colors.deepOrangeAccent;
   static final background = hexToColor('#F6F6F6');
   static final background2 = hexToColor('#F8FAFF');
