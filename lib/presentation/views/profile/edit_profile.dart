e17392143419c40458d0d9e1611e5b7ec54b3402
Nick <nick_11-13@hotmail.com>
2024-08-15 23:32:02 +0700

    Revert "git1"
    This reverts commit 2de6c440d6ee4918599cc96c9530de9ed9de5f9c.

--- lib/presentation/views/profile/edit_profile.dart
+++ lib/presentation/views/profile/edit_profile.dart
@@ -63,7 +63,7 @@ class EditProfilePage extends StatelessWidget {
                       style:
                           TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Text('KoratKorat, Thailand'),
+                    Text('Korat, Thailand'),
                     SizedBox(height: 15),
                     ElevatedButton(
                       onPressed: () {
